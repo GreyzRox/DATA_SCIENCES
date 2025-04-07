c258c43091cde67cae3a8065b542817cc7b0092e
GreyzRox <guillaume.dupart.pro@gmail.com>
2025-04-07 10:28:06 +0200

    updaaate

---
+++ iads/.ipynb_checkpoints/evaluation-checkpoint.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package: iads
+File: evaluation.py
+Année: LU3IN026 - semestre 2 - 2024-2025, Sorbonne Université
+"""
+
+# ---------------------------
+# Fonctions d'évaluation de classifieurs
+
+# import externe
+import numpy as np
+import pandas as pd
+
+# ------------------------ 
+def crossval(X, Y, n_iterations, iteration):
+    start = int(iteration *(X.shape[0]//n_iterations))
+    end = int((iteration + 1) *(X.shape[0]//n_iterations))
+    Xtest = X[start:end]
+    Ytest = Y[start:end]
+    Xapp = np.concatenate((X[:start],X[end:]))
+    Yapp = np.concatenate((Y[:start],Y[end:]))
+
+    return Xapp, Yapp, Xtest, Ytest  
+    return Xapp, Yapp, Xtest, Ytest
+
+##############################################################################
+
+def crossval_strat(X, Y, n_iterations, iteration):
+    #############
+    # A COMPLETER
+    #############
+    classe = np.unique(Y) 
+    indices_classe = {c: np.where(Y==c)[0] for c in classe}
+    test_indices= []
+    train_indices =[]
+    for c in classe:
+        indices = indices_classe[c]  # Indices de la classe actuelle
+        taille_test = len(indices) // n_iterations 
+        start =iteration*taille_test
+        end=(iteration+1)*taille_test
+        test_indices.extend(indices[start:end]) 
+        train_indices.extend(np.concatenate((indices[:start], indices[end:]))) 
+
+    Xtest,Ytest=X[test_indices],Y[test_indices]
+    Xapp,Yapp=X[train_indices],Y[train_indices]
+    return Xapp, Yapp, Xtest, Ytest
+
+##############################################################################
+
+def analyse_perfs(L):
+    """ L : liste de nombres réels non vide
+        rend le tuple (moyenne, écart-type)
+    """
+    # np.std rend l'écart type
+    return (np.mean(L), np.std(L))
+    
+##############################################################################
+
+import copy  # pour deepcopy()
+
+def validation_croisee(C, DS, nb_iter):
+    """ Classifieur * tuple[array, array] * int -> tuple[ list[float], float, float]
+    """
+    
+    perf = []
+
+    for i in range(nb_iter):
+        perceptron = copy.deepcopy(C)
+        Xapp,Yapp,Xtest,Ytest = crossval_strat(DS[0], DS[1], nb_iter, i)
+        perceptron.train(Xapp,Yapp)
+        accuracy=perceptron.accuracy(Xtest,Ytest)
+        perf.append(accuracy)
+        
+        print(f"Taux de bonne classification moyen sur {nb_iter} itérations : {np.mean(perf):.4f}%") 
+    moyenne, ecart_type = analyse_perfs(perf)
+
+    return perf,moyenne,ecart_type
+    
+##############################################################################
+
+# ------------------------ 
+
